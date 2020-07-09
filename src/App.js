537d0047664a66bcd9d0b62bf7e918be32c99b20
tommyrharper <tommyrharper@gmail.com>
2020-07-09 16:30:00 +0100

    Add Todo function to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+function Todo({ todo, index }) {
+  return <div className="todo">{todo.text}</div>;
+}
 
 function App() {
   const [todos, setTodos] = useState([
     {
-      text: 'Learn about React',
-      isCompleted: false
+      text: "Learn about React",
+      isCompleted: false,
     },
     {
-      text: 'Meet friend for lunch',
-      isCompleted: false
+      text: "Meet friend for lunch",
+      isCompleted: false,
     },
     {
-      text: 'Build todo app',
-      isCompleted: false
-    }
-  ])
+      text: "Build todo app",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div classNmae="app">
+      <div classname="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;

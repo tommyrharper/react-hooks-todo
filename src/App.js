b53eebb073ac07a2b19a7f9dfefb9c4ba1537804
tommyrharper <tommyrharper@gmail.com>
2020-07-09 16:54:44 +0100

    Add completeTodo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Todo({ todo, index }) {
-  return <div className="todo">{todo.text}</div>;
+function Todo({ todo, index, completeTodo }) {
+  return <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className="todo">
+    {todo.text}
+    <div>
+      <button onClick={() => completeTodo(index)}>Complete</button>
+    </div>
+  </div>;
 }
 
 function TodoForm({ addTodo }) {
@@ -49,11 +54,17 @@ function App() {
     setTodos(newTodos);
   };
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>

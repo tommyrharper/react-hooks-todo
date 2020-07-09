02f6e4de3af1d5055a8d738f293e33f8ec14db5e
tommyrharper <tommyrharper@gmail.com>
2020-07-09 16:48:39 +0100

    Allow adding new Todos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ function Todo({ todo, index }) {
   return <div className="todo">{todo.text}</div>;
 }
 
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -21,12 +38,18 @@ function App() {
     },
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, {text}];
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
